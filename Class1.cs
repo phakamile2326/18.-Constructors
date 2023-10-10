063a6dd8ae86c0f026bada559034953afe0d2424
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-10 15:02:49 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            book book1 = new book("Harry Porter", "JK Rowling", 400);
+
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    internal class book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        //this method below is callaed a constructor, it is called by default when a book
+        public book(string title, string author, int pages)
+        {
+            title = "aTitle";
+            author = "aAuthor";
+            pages = 200;
+        }
+    }
+}
